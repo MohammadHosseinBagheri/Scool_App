23a20f40815e68b3c4d38bd20518bc2900fc69fe
MohammadHosseinBagheri <mohammadbagheri228@gmail.com>
2019-09-03 17:57:31 -0700

    add loading when openning helpemail page

--- src/pages/HelpEmail.js
+++ src/pages/HelpEmail.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text ,StyleSheet} from 'react-native';
-import { Card, CardItem, Thumbnail,Icon } from 'native-base';
+import { View, Text, StyleSheet } from 'react-native';
+import { Card, CardItem, Thumbnail, Icon, Button } from 'native-base';
 import { FlatList } from 'react-native-gesture-handler';
+import LottieView from 'lottie-react-native';
 import MyHeader from '../components/MyHeader';
 class HelpEmail extends Component {
     constructor(props) {
@@ -15,36 +16,49 @@ class HelpEmail extends Component {
     }
     render() {
         return (
-            <View style={{ flex: 1,backgroundColor:'#455A64' }}>
+            <View style={{ flex: 1, backgroundColor: '#455A64' }}>
                 <MyHeader
                     left={<Icon name={'arrow-back'} style={{ color: 'white' }} onPress={() => {
                         this.props.navigation.goBack()
                     }} />}
-                    right={<Icon name={'mail'} style={{ color: 'white' }} />}
+                    right={
+                        <Button transparent onPress={() => {
+                            this.props.navigation.openDrawer()
+                        }} >
+                            <LottieView style={{ width: 40 }} source={require('../animation/email.json')} autoPlay loop
+
+                            />
+                        </Button>}
                     body={<Text style={{ fontFamily: 'IRANSansMobile', color: 'white', fontSize: 16 }} > پشتیبانی و ایمیل    </Text>}
                 />
-                <FlatList data={this.state.Teachers}
-                    renderItem={({ item, index }) => (
-                        <Card>
-                            <CardItem header bordered style={{backgroundColor:'#FFF59D'}} >
-                                <Thumbnail source={{uri:'https://pickaface.net/assets/images/slides/slide2.png'}} />
-                                <View style={{ flex: 1 }}>
-                                    <Text style={[styles.cardBodyText,{ fontFamily: 'IRANSansMobile_Light', elevation: 5 }]}>
-                                        آقای {item.first_name} {item.last_name}
-                                    </Text>
-                                </View>
-                            </CardItem>
-                            <CardItem cardBody style={{backgroundColor:'#FF8A65'}}>
-                                <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-                                    <Text style={styles.cardBodyText}>
-                                        ایمیل : {item.email}
-                                    </Text>
-                                </View>
-                            </CardItem>
-                        </Card>
-                    )}
-                    keyExtractor={({ item, index }) => (index)}
-                />
+                {
+                    this.state.Teachers.length == 0
+                        ?
+                        (<LottieView style={{ flex: 1 }}  source={require('../animation/loading2.json')} autoPlay loop />)
+                        :
+                        (<FlatList data={this.state.Teachers}
+                            renderItem={({ item, index }) => (
+                                <Card>
+                                    <CardItem header bordered style={{ backgroundColor: '#FFF59D' }} >
+                                        <Thumbnail source={{ uri: 'https://pickaface.net/assets/images/slides/slide2.png' }} />
+                                        <View style={{ flex: 1 }}>
+                                            <Text style={[styles.cardBodyText, { fontFamily: 'IRANSansMobile_Light', elevation: 5 }]}>
+                                                آقای {item.first_name} {item.last_name}
+                                            </Text>
+                                        </View>
+                                    </CardItem>
+                                    <CardItem cardBody style={{ backgroundColor: '#FF8A65' }}>
+                                        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                                            <Text style={styles.cardBodyText}>
+                                                ایمیل : {item.email}
+                                            </Text>
+                                        </View>
+                                    </CardItem>
+                                </Card>
+                            )}
+                            keyExtractor={({ item }) => (item)}
+                        />)
+                }
             </View>
         );
     }
